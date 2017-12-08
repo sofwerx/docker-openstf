4c09510507df178d7d7e9f6c98b6cece921b99d7
Mike Ahern <mike.ahern@sofwerx.org>
2017-12-08 14:58:45 -0500

    This will install Docker on PopOS

---
+++ ubuntu17.10-docker-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce -y
